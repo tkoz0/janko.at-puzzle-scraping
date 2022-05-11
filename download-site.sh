4cd9800fc7505a05fb1bb7bc7ff0e837191db5eb
null <null>
2022-05-11 16:24:53 -0400

    better website download command

--- download-site.sh
+++ download-site.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-wget --recursive --no-clobber --page-requisites --html-extension --convert-links --restrict-file-names=windows --domains janko.at --no-parent https://www.janko.at/
+# -m = mirror options
+# -k = convert links (for offline viewing)
+# -p = page requisites (download all files needed to display page)
+# -D = domain list
+# -np = no parent
+# -E = adjust extension (use .htm(l) for pages saved locally)
+wget -m -k -p -D janko.at -np -E --restrict-file-names=windows https://www.janko.at/
